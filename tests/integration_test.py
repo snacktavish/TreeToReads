6117e4639c3cb53285977eea75907c70e3b38b1a
Emily Jane McTavish <ejmctavish@gmail.com>
2014-11-18 17:15:59 +0100

    verrrrry basic tests

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,6 +1,12 @@
 import unittest
+from treetoreads import TreeToReads
 
+class PrimesTestCase(unittest.TestCase):
+    """Tests for `primes.py`."""
 
-class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(fun(3), 4)
+    def test_is_five_prime(self):
+        """Is five successfully determined to be prime?"""
+        self.assertTrue(TreeToReads(configfi='tests/input/test1.cfg',run=0))
+
+if __name__ == '__main__':
+    unittest.main()
