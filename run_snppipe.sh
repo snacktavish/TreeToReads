00f1af0b1d0e711959e198d2023eb0c33a74943a
Emily Jane McTavish <ejmctavish@gmail.com>
2014-12-30 15:29:01 -0500

    fixed varnames

--- run_snppipe.sh
+++ run_snppipe.sh
@@ -1,22 +1,22 @@
 
 TTR_OUT=/home/ejmctavish/Documents/FDA/TreetoReads/test_out
-PIPEFI=cfsan_snppipe_input
+DATA=cfsan_snppipe_input
 REF=/home/ejmctavish/Documents/FDA/TreetoReads/example/barref.fasta
 
 cd $TTR_OUT
 
-mkdir $PIPEFI
+mkdir $DATA
 
 for fi in `ls ${TTR_OUT}/*_1.fq | cut -d"," -f2`
     do 
         fullp=${fi%_*}
         filename=${fullp##*/}
         echo $filename
-        mkdir $PIPEFI/$filename
-        cp ${fullp}_1.fq $PIPEFI/$filename
-        cp ${fullp}_2.fq $PIPEFI/$filename
+        mkdir $DATA/$filename
+        cp ${fullp}_1.fq $DATA/$filename
+        cp ${fullp}_2.fq $DATA/$filename
         
     done
 
     
-run_snp_pipeline.sh -s $PIPEFI $REF
+run_snp_pipeline.sh -s $DATA $REF
