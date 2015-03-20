df03fba376ac135323fcbd71cd8e80fbee7d204c
Emily Jane McTavish <ejmctavish@gmail.com>
2015-03-20 11:52:08 +0100

    tales cmd line args

--- run_snppipe.sh
+++ run_snppipe.sh
@@ -1,11 +1,13 @@
+#!/bin/sh
 
-TTR_OUT=/home/ejmctavish/Documents/FDA/TreetoReads/test_out
-DATA=cfsan_snppipe_input
-REF=/home/ejmctavish/Documents/FDA/TreetoReads/example/barref.fasta
 
-cd $TTR_OUT
+TTR_OUT=$1
+REF=$2
+DATA=cfsan_snppipe_input_test
 
-mkdir $DATA
+#CHECK ARGS!
+
+#mkdir $DATA #How to make this a try statement?
 
 for fi in `ls ${TTR_OUT}/*_1.fq | cut -d"," -f2`
     do 
@@ -19,4 +21,4 @@ for fi in `ls ${TTR_OUT}/*_1.fq | cut -d"," -f2`
     done
 
     
-run_snp_pipeline.sh -s $DATA $REF
+#run_snp_pipeline.sh -s $DATA $REF # need to remeber how to install SNP pipeline...
