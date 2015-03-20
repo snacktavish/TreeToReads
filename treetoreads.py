acc8942d74523d41a4e9e92319503d8a613ef740
Emily Jane McTavish <ejmctavish@gmail.com>
2015-03-20 11:51:35 +0100

    speeded up mutation picking, and re-worked arg reading

--- treetoreads.py
+++ treetoreads.py
@@ -6,6 +6,7 @@ import os
 import sys
 import argparse
 import numpy
+import sys
 
 
 class TreeToReads:
@@ -18,22 +19,22 @@ class TreeToReads:
   _genmut=0
   _genread=0
   _vargen=0
-  def __init__(self, configfi='seqsim.cfg', run=1, clustering=0):
+  def __init__(self, configfi='seqsim.cfg', run=1):
         """A method to read a tree, resolve polytomes, generate mutations and simulate reads."""
         self.configfi=configfi
         self.run=run
-        self.clustering=clustering
         if self.run:
           self.runSims()
+        self._checkArgs()
   def runSims(self):
             self.runART()      
   def readArgs(self):
     print("reading arguments")
     self._argread=1
     if len(sys.argv) > 1:
-      conf=sys.argv[1]
+      self.configfi=sys.argv[1]
       try:
-        config=open(conf).readlines()
+        config=open(self.configfi).readlines()
       except:
         print("couldn't open config file {}".conf)
         sys.exit()
@@ -52,10 +53,19 @@ class TreeToReads:
       if not self._argread:
         self.readArgs()
       self._madeout=1
+      print('output directory is {}'.format(self.outd))
       if not os.path.isdir(self.getArg('outd')):
           os.mkdir(self.getArg('outd'))
       self.bashout = open('{}/analysis.sh'.format(self.getArg('outd')),'w')
+      configout = open('{}/analysis_configuration.cfg'.format(self.getArg('outd')),'w')
+      for lin in open(self.configfi).readlines():
+          configout.write('lin')
+      configout.close()
   def getArg(self,nam):
+    try:
+      len(self.argdict)
+    except:
+      self.readArgs()
     if nam in self.argdict:
         return(self.config[self.argdict[nam]])
     else:
@@ -65,21 +75,24 @@ class TreeToReads:
         return(None)
   def _checkArgs(self):
     print("checking args")
+    if self._argread!=1:
+      self.readArgs()
     self.argdict={'treepath':'treefile_path', 'nsnp':'number_of_snps', 'anchor_name':'anchor_name', 'genome':'anchor_genome_path', 'ratmat':'rate_matrix', 'freqmat':'freq_matrix', 'shape':'shape', 'errmod1':'error_model1', 'errmod2':'error_model2', 'cov':'coverage', 'outd':'output_dir'}
     for arg in self.argdict:
       if self.argdict[arg] not in self.config:
         print("{} is missing from the config file".format(self.argdict[arg]))
         sys.exit()
     try:
-      int(self.getArg('nsnp'))
+        self.nsnp=int(self.getArg('nsnp'))
+        print('Number of SNPS is {}'.format(self.nsnp))
     except:
         print("number of SNPs {} could not be coerced to an integer".format(self.getArg('nsnp')))
         sys.exit()
     if not len(self.getArg('ratmat').split(','))==6:
-        print("{} values in rate matrix, should be 6".format(len(self.getArg('ratmat').split(','))))
+        print("{} values in rate matrix, there should be 6".format(len(self.getArg('ratmat').split(','))))
         sys.exit()
     if not len(self.getArg('freqmat').split(','))==4:
-        print("{} values in freq matrix, should be 4".format(len(self.getArg('freqmat').split(','))))
+        print("{} values in freq matrix, there should be 4".format(len(self.getArg('freqmat').split(','))))
         sys.exit()  
     try:
         float(self.getArg('shape'))
@@ -96,14 +109,25 @@ class TreeToReads:
     except:
         print("could not open anchor genome {}".format(self.getArg('genome')))
         sys.exit() 
+    try:
+        self.outd=self.getArg('outd')
+    except:
+        self.outd=('ttr_out')
     if self.getArg('mutation_clustering') is not None:
       if self.getArg('mutation_clustering')=='ON':
+        self.clustering=1
         try:
             self.clustPerc = float(self.getArg('percent_clustered'))
             self.lambd = float(self.getArg('exponential_lambda'))
         except:
             print("Problem reading clustering parameters, requires number for 'percent_clustered' and 'exponential_lambda'")
             sys.exit()
+      else: 
+        print('Mutation clustering is OFF, to use set mutation_clustering = ON and values for "percent_clustered" and "exponential_lambda"')
+        self.clustering=0
+    else: 
+        print('Mutation clustering is OFF')
+        self.clustering=0
   def readTree(self):
     print("read tree")
     self._treeread=1
@@ -129,6 +153,7 @@ class TreeToReads:
     crop=[lin[:-1] for lin in genfas[1:]]
     self.gen="".join(crop)
     self.genlen=len(self.gen)
+    print("Genome has {} bases".format(self.genlen))
   def generateVarsites(self):
     print("gen varsites")
     self._vargen=1
@@ -136,7 +161,8 @@ class TreeToReads:
     if not self._treeread:
       self.readTree()
     self.simloc="{}/seqs_sim.txt".format(self.getArg('outd'))
-    seqcall=" ".join(['seq-gen', '-l{}'.format(10*int(self.getArg('nsnp'))), '-n1', '-mGTR', '-a{}'.format(self.getArg('shape')), '-r{}'.format(self.getArg('ratmat')), '-f{}'.format(self.getArg('freqmat')), '-or','<', '{}'.format(self.outtree),'>', '{}'.format(self.simloc)])
+    lenseqgen=10*int(self.getArg('nsnp'))
+    seqcall=" ".join(['seq-gen', '-l{}'.format(lenseqgen), '-n1', '-mGTR', '-a{}'.format(self.getArg('shape')), '-r{}'.format(self.getArg('ratmat')), '-f{}'.format(self.getArg('freqmat')), '-or','<', '{}'.format(self.outtree),'>', '{}'.format(self.simloc)])
     os.system(seqcall)
     self.bashout.write(seqcall +'\n')
   def readVarsites(self):
@@ -180,7 +206,7 @@ class TreeToReads:
           site[srr]=simseqs[srr][i]
           nucs.add(simseqs[srr][i])
       assert(len(nucs)>1)
-      self.sitepatts[nuc].append(site)
+      self.sitepatts[nuc].append(site)  #PICKLE THIS SOMEHOW?!?!
   def selectMutsites(self):
     print("select mutsites")
     if not self._madeout: 
@@ -190,29 +216,27 @@ class TreeToReads:
     self.mutsite="{}/mutsites.txt".format(self.getArg('outd'))
     fi=open(self.mutsite,"w")
     rands=set()
-    i=0
-    while i < int(self.getArg('nsnp')): #This is the number of SNPs
-      if i%100==0:
-        print('.')
-      if self.clustering and i < (int(self.getArg('nsnp'))*self.clustPerc):
-            i+=2
-            ran=random.choice(range(self.genlen))
-            rands.add(ran)
-            fi.write(str(ran)+'\n')
+    if self.clustering:
+        ranpairA=random.sample(range(self.genlen),(self.nsnp*self.clustPerc)/2)
+        rands.add(set(ranpairA))
+        for site in ranpairA:
             diff = 0 #1+exponential ... 
             while diff==0: #RISKY at HIGH LAMBDA!
                 diff=int(random.expovariate(self.lambd))
-            if (random.choice([0, 1]) or (ran-diff < 0)) and (ran+diff < self.genlen):
-              ranpair=ran+diff
+            if (random.choice([0, 1]) or (site-diff < 0)) and (site+diff < self.genlen):
+              ranpairB=site+diff
             else:
-              ranpair=ran-diff #Can run off of the end of the genome...
-            rands.add(ranpair)
-            fi.write(str(ranpair)+'\n')
-      else:
-            i+=1
-            ran=random.choice(range(self.genlen))
-            rands.add(ran)
-            fi.write(str(ran)+'\n')
+              ranpairB=site-diff
+            rands.add(ranpairB)
+        ransingle=random.sample(range(self.genlen),self.nsnp*(1-self.clustPerc))
+        rands.add(set(ransingle))# TODO check this is legal
+        for site in rands:
+                fi.write(str(site)+'\n')
+    else:
+            ran=random.sample(range(self.genlen),self.nsnp) # Makes sure this works..., test if is sampling with replacement or no. Prefer W/O replacement.
+            rands=set(ran)
+            for site in rands:
+                fi.write(str(site)+'\n')
     print("realized number of mutations is {}".format(len(rands)))
     self.mutlocs=rands
     # set of random numbers determining where the SNPs really fall. can be drawn from SNPlist or just random.
@@ -242,7 +266,7 @@ class TreeToReads:
                    genout.write('\n')
                 ii+=1
                 if ii in self.mutlocs:
-                   patt=random.choice(self.sitepatts[nuc]) # risky at nigh num taxa? or too few varsites - all will have same pattern...
+                   patt=random.choice(self.sitepatts[nuc]) # risky at nigh num taxa? or too few varsites - all will have same pattern... But won't run out of sites...
                    genout.write(patt[seq])
                    self.mut_genos[seq].append(patt[seq])
                 else:
@@ -271,9 +295,6 @@ class TreeToReads:
       #Get distributaion of SNP locations from real data?!!
       # simulate reads for each of those genomes.
       """
-
-
-
 
 if __name__ == "__main__":
   ttr=TreeToReads()
