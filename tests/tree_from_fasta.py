29ca50975ecfbc2e642049c26fb0186161492edb
Emily Jane McTavish <ejmctavish@gmail.com>
2015-04-12 20:24:38 +0200

    test tree from fasta is correct

---
+++ tests/tree_from_fasta.py
@@ -0,0 +1,37 @@
+import unittest
+from treetoreads import TreeToReads
+import subprocess
+import dendropy
+import subprocess
+import os
+
+
+def compare_trees(expected,estimated):
+ #   assert(estimated.euclidean_distance(expected)<= branch lengths are hard to test. TODO figure out how.
+    taxon_namespace = dendropy.TaxonSet()
+    exp_tree = dendropy.Tree.get_from_path(
+        expected,
+        "newick",
+        taxon_set=taxon_namespace)
+    est_tree = dendropy.Tree.get_from_path(
+        estimated,
+        "nexus",
+        taxon_set=taxon_namespace)
+    return(est_tree.symmetric_difference(exp_tree))
+
+if __name__ == '__main__':
+    ttr=TreeToReads(configfi='/home/ejmctavish/Documents/FDA/TreetoReads/tests/input/tree_from_fasta.cfg',run=0)
+    print("outd is {}".format(ttr.outd))
+    ttr.outd="/home/ejmctavish/Documents/FDA/TreetoReads/tests/tree_from_fasta"
+    ttr.makeOut()
+    ttr.mutGenomes()
+    print(ttr.getArg('genome'))
+    # enter the directory like this:
+    os.system("cat {}/*.fasta > {}/mini.aln".format(ttr.outd, ttr.outd))
+    os.system("Garli tests/garli_mini.conf")
+    assert(compare_trees('tests/input/simple_expected.tre','tests/tree_from_fasta/tff.best.tre')==0)
+    assert(compare_trees('tests/input/alt.tre','tests/tree_from_fasta/tff.best.tre')!=0)
+    print('Topology is correct')
+
+
+
