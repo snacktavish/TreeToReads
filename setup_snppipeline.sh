14d4353ec80c8a016314c679e3a7f3462f2daaf3
Emily Jane McTavish <ejmctavish@gmail.com>
2015-04-12 21:48:33 +0200

    snp pipeline still not running

--- setup_snppipeline.sh
+++ setup_snppipeline.sh
@@ -6,25 +6,25 @@ REF=$2
 DATA=cfsan_snppipe_input_test
 
 #CHECK ARGS!
+cd $TTR_OUT
+mkdir -p $DATA #How to make this a try statement?
 
-#mkdir $DATA #How to make this a try statement?
-
-for fi in `ls ${TTR_OUT}/*_1.fq | cut -d"," -f2`
+for fi in `ls *_1.fq | cut -d"," -f2`
     do 
         fullp=${fi%_*}
         filename=${fullp##*/}
         echo $filename
-        mkdir $DATA/$filename
-        cp ${fullp}_1.fq $TTR_OUT/$DATA/$filename
-        cp ${fullp}_2.fq $TTR_OUT/$DATA/$filename
+        mkdir -p $DATA/$filename
+        cp ${fullp}_1.fq $DATA/$filename
+        cp ${fullp}_2.fq $DATA/$filename
         
     done
 
     
-run_snp_pipeline.sh -s $TTR_OUT/$DATA -o $TTR_OUT/$DATA $REF
+run_snp_pipeline.sh -s $DATA -o nee ../$REF
 
-cp example/garli.conf $TTR_OUT/$DATA
+cp ../example/garli.conf $DATA
 
-cd $TTR_OUT/$DATA
+cd $DATA
 
 Garli 
