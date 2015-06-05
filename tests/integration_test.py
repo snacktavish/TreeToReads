2d758222664ed9bff55f103b63172c99c248859e
Emily Jane McTavish <ejmctavish@gmail.com>
2015-06-05 12:07:48 +0200

    simple full run

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -1,20 +1,22 @@
 import unittest
 from treetoreads import TreeToReads
 
-class IntegrationTestCase(unittest.TestCase):
-    """Tests that it run`."""
+#class IntegrationTestCase(unittest.TestCase):
+#    """Tests that it run`."""
 
-    def can_run(self):
-        """Does it run?"""
-        self.assertTrue(TreeToReads(configfi='tests/input/test1.cfg',run=1))
+ #   def can_run(self):
+ #       """Does it run?"""
+ #       self.assertTrue(TreeToReads(configfi='tests/input/test1.cfg',run=1))
 
-    def correct_snp_num(self):
-    	"""Is the expected number of snps getting created?"""
-    	ttr=TreeToReads(configfi='tests/input/test1.cfg',run=0)
-    	ttr.selectMutsites()
+ #   def correct_snp_num(self):
+ #   	"""Is the expected number of snps getting created?"""
+ #   	ttr=TreeToReads(configfi='tests/input/test1.cfg',run=0)
+ #   	ttr.selectMutsites() 
 #        assert(len(ttr.rands))
 
 
 if __name__ == '__main__':
     ttr=TreeToReads(configfi='tests/input/test1.cfg',run=0)
+    ttr.runART()
+    ttr=TreeToReads(configfi='seqsim.cfg',run=0)
     ttr.runART()
