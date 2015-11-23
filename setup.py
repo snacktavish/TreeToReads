eb9f495e9be5f61b1136a4ae606c4f32b088323c
Emily Jane McTavish <ejmctavish@gmail.com>
2015-11-22 18:01:44 -0600

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='TreeToReads',
+    version='0.1dev',
+    py_modules =['treetoreads',],
+    license='No copyright',
+    long_description=open('README.md').read(),
+)
