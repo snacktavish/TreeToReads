6f49f3047176ca35c4cd49e86478ba8858249abd
Emily Jane McTavish <ejmctavish@gmail.com>
2015-11-29 20:50:18 -0600

    tidying

--- setup.py
+++ setup.py
@@ -5,5 +5,9 @@ setup(
     version='0.1dev',
     py_modules =['treetoreads',],
     license='No copyright',
+    description = 'A random test lib',
+    author = 'Emily Jane McTavish  ',
+    author_email = 'ejmctavish@gmail.com',
+    url = 'https://github.com/snacktavish/TreeToReads', # use the URL to the github repo
     long_description=open('README.md').read(),
 )
