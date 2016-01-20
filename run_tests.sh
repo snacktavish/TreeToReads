48741893ce92564881497c5e37b3c5565d6aee35
Emily Jane McTavish <ejmctavish@gmail.com>
2016-01-19 19:37:31 -0600

    minor test improvements

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,5 @@ echo "Running basic tests"
 
 echo "tree reconstruction from fasta"
 python tests/tree_from_fasta.py
-echo "simple clustering run"
-python tests/clustering.py
 echo "integration"
 python tests/integration_test.py
