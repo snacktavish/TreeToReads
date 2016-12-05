e94cbe73418e21ae63ec00b156c5318c848d924a
Emily Jane McTavish <ejmctavish@gmail.com>
2016-12-05 11:27:10 -0800

    tests

---
+++ tests/check_refgenome.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import dendropy
+
+import sys
+ref = sys.argv[1]
+sim = sys.argv[2]
+
+inputseq = dendropy.DnaCharacterMatrix.get(file=open(ref), schema="fasta")
+outputseq = dendropy.DnaCharacterMatrix.get(file=open(sim), schema="fasta")
+
+assert len(inputseq) == len(outputseq)
+i = 0
+for item in inputseq: 
+  assert(inputseq[i].symbols_as_string() == outputseq[i].symbols_as_string().replace('-',''))
+  i+=1
+
+print "test passed\n"
