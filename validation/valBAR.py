9d8162a3318ab34299e79d4ea61b0bab421267a6
Emily Jane McTavish <ejmctavish@gmail.com>
2016-12-19 10:27:40 -0800

    check coverage

--- validation/valBAR.py
+++ validation/valBAR.py
@@ -7,24 +7,40 @@ refloc =  "example/barref.fasta"
 altrefloc = 'validation/LT2.fasta'
 num = 5
 
-#perform_sims(config, dirstub, num)
+perform_sims(config, dirstub, num)
 
-#for i in range(num):
-#        i+=1
-#        diri = "{}{}".format(dirstub,i)
-#        os.system("mkdir {}/altref".format(diri))
-#        os.system("cp -r {}/fastq {}/altref/fastq".format(diri, diri))
+for i in range(num):
+        i+=1
+        diri = "{}{}".format(dirstub,i)
+        os.system("mkdir {}/altref".format(diri))
+        os.system("cp -r {}/fastq {}/altref/fastq".format(diri, diri))
 
 
-#run_snppipe(dirstub, refloc, num)
+run_snppipe(dirstub, refloc, num)
 
 #alt refreence genome
 
-#for i in range(num):
-#    i+=1
-#    diri = "{}{}".format(dirstub,i)
-#    os.system("run_snp_pipeline.sh -f -s {}/altref/fastq -o {}/altref {}".format(diri, diri, altrefloc)) 
+for i in range(num):
+    i+=1
+    diri = "{}{}".format(dirstub,i)
+    os.system("run_snp_pipeline.sh -f -s {}/altref/fastq -o {}/altref {}".format(diri, diri, altrefloc)) 
 
 inf_dict = perform_analyses(dirstub, num)
 alt_inf_dict = perform_analyses_altref(dirstub, num)
 
+
+
+for i in range(num):
+    i+=1
+    diri = "{}{}".format(dirstub,i)
+    fi = open("{}/metrics.tsv".format(diri))
+    a = fi.readline()
+    print fi.readline().split('\t')[8]
+
+
+for i in range(num):
+    i+=1
+    diri = "{}{}/altref".format(dirstub,i)
+    fi = open("{}/metrics.tsv".format(diri))
+    a = fi.readline()
+    print fi.readline().split('\t')[8]
--- validation/valLM.py
+++ validation/valLM.py
@@ -6,8 +6,15 @@ config = "validation/lm/Lm.config"
 refloc = "validation/lm/CFSAN023463.fasta"
 num = 5
 
-#perform_sims(config, dirstub, num)
-#run_snppipe(dirstub, refloc, num)
+perform_sims(config, dirstub, num)
+run_snppipe(dirstub, refloc, num)
 perform_analyses(dirstub, num)
+
+for i in range(num):
+     i+=1
+     workdir = "{}{}".format(dirstub,i)
+     check_indel_locs(diri, 'CFSAN023463_sim_')
+ 
+
 
 
