62d947c5abca53306861624a4d18e21001e2f073
willpitchers <willpitchers@gmail.com>
2018-09-27 14:45:40 +1000

    added make_isolates_list.sh

---
+++ make_isolates_list.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+## This script prepares an `isolates.tab` file as an input for a `nullarbor` run on simulated data
+
+# move to TTS output directory
+TTS_outdir=$1 
+cd TTS_outdir
+
+# prepare isolates.tab
+ls -d fastq/* | cut -d'/' -f 2 > isolist
+ls fastq/*/*1.fq.gz > read1
+ls fastq/*/*2.fq.gz > read2
+
+paste isolist read1 read2 > ../isolates.tab 
+
+rm isolist read1 read2
